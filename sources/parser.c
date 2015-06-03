dae2bd60ce1771270bcadbd66c5be604cb002a58
Kevin COULIBALY <kcouliba@e1r1p3.42.fr>
2015-06-03 21:48:08 +0200

    parser started

--- sources/parser.c
+++ sources/parser.c
@@ -6,8 +6,16 @@
 /*   By: kcouliba <kcouliba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/29 16:29:50 by kcouliba          #+#    #+#             */
-/*   Updated: 2015/05/29 16:40:06 by kcouliba         ###   ########.fr       */
+/*   Updated: 2015/06/03 21:47:38 by kcouliba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
+
+size_t	parser(t_env *e, char *filename)
+{
+	(void)e;
+	if (!filename)
+		return (EXIT_ERROR);
+	return (EXIT_SUCCESS);
+}
